3e04e5cd174f23a79648474d6030b8fccd4dd936
Ville <ville.nieminen0@gmail.com>
2022-08-07 23:32:07 +0300

    Simplify c code

--- src/signals/signal_handling.c
+++ src/signals/signal_handling.c
@@ -2,12 +2,12 @@
 #include "signal.h"
 #include "stddef.h"
 
-int8_t signals_listen(void (*handler)(int)) {
+int signals_listen(void (*handler)(int)) {
   struct sigaction signal_action = {.sa_handler = handler, .sa_flags = 0};
 
-  return (int8_t){sigemptyset(&signal_action.sa_mask) +
-                  sigaction(SIGHUP, &signal_action, NULL) +
-                  sigaction(SIGINT, &signal_action, NULL) +
-                  sigaction(SIGQUIT, &signal_action, NULL) +
-                  sigaction(SIGTERM, &signal_action, NULL)};
+  return sigemptyset(&signal_action.sa_mask) +
+         sigaction(SIGHUP, &signal_action, NULL) +
+         sigaction(SIGINT, &signal_action, NULL) +
+         sigaction(SIGQUIT, &signal_action, NULL) +
+         sigaction(SIGTERM, &signal_action, NULL);
 }
--- src/signals/signal_handling.h
+++ src/signals/signal_handling.h
@@ -1,7 +1,5 @@
-#include <stdint.h>
-
 /*
  * Sets handler for signals that should quit the program. Returns 0 on success
  * or a negative value to indicate error.
  */
-int8_t signals_listen(void (*handler)(int));
+int signals_listen(void (*handler)(int));
